feb1953c2ff0267c2cea1e665b9786e853b1a108
Pedro <pedro795027@gmail.com>
2023-11-10 12:42:42 -0300

    nada faz sentido

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,6 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from "../screens/HomeScreen";
 import CardsScreen from "../screens/CardsScreen";
 import BankStatementScreen from "../screens/BankStatementScreen";
 
@@ -9,7 +8,6 @@ const { Navigator, Screen } = createNativeStackNavigator();
 export default function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name='HomeScreen' component={HomeScreen} />
       <Screen name='CardsScreen' component={CardsScreen} />
       <Screen name='BankStatementScreen' component={BankStatementScreen} />
     </Navigator>
