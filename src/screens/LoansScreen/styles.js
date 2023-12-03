bb7d2c4f8fa14e17d1807449c51c0f338a05563b
Pedro <pedro795027@gmail.com>
2023-12-03 19:19:11 -0300

    Feat: crição da tela "Empréstimo"

--- src/screens/LoansScreen/styles.js
+++ src/screens/LoansScreen/styles.js
@@ -9,6 +9,30 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.BACKGROUND,
     flex: 1,
   },
+  cardLoan:{
+    marginTop: '10%',
+    padding: 20,
+    width: '90%',
+    height: '75%',
+    backgroundColor: theme.colors.SHAPE,
+    borderRadius: 20,
+    justifyContent:'center',
+    alignItems: 'center', 
+  },
+  cardContainer:{
+    justifyContent:'center',
+    alignItems: 'center', 
+  },
+  textLoan:{
+    fontFamily: theme.fonts.SEMI_BOLD,
+    color: theme.colors.TEXT,
+    fontSize: 15,
+  },
+  textValue: {
+    fontFamily: theme.fonts.SEMI_BOLD,
+    color: theme.colors.SECONDARY,
+    fontSize: 40,
+  }
 })
 
 export default styles;
